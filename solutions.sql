bdb6b4f99cbc247b5acc757e665688198b936c12
Michael Taggart <mtaggarthawk@gmail.com>
2020-12-07 16:32:02 -0700

    initial commit

---
+++ solutions.sql
@@ -0,0 +1,40 @@
+-- On the left are the Tables with their fields. The right is where we will be writing our queries. The bottom is where we will see our results.
+
+-- Step 1
+-- Instructions
+-- SELECT all the data FROM the artist table.
+
+-- Solution
+--  SQL Solution
+-- Step 2
+-- Instructions
+-- SELECT the first_name, last_name, and country FROM the employee table.
+
+-- Solution
+--  SQL Solution
+-- Step 3
+-- Instructions
+-- SELECT the name, composer, and milliseconds FROM the track table WHERE the milliseconds are greater than 299000.
+
+-- Solution
+--  SQL Solution
+-- Step 4
+-- Instructions
+-- SELECT the count FROM the track table WHERE the milliseconds are greater than 299000.
+
+-- Solution
+--  SQL Solution
+-- Black Diamond
+-- Now that we have some basic query examples. Let's try doing some more complicated ones. Use www.sqlteaching.com or sqlbolt.com as resources for the missing keywords you'll need.
+
+-- Find the average length of all tracks in milliseconds
+-- Find the number of invoices in the USA
+-- Make a list of all the First Names of Customers that contain an 'a'
+-- Make a list of the 10 longest tracks
+-- Make a list of the 20 shortest tracks
+-- Find all the customers that live in California or Washington
+-- Find all the customers that live in California, Washington, Utah, Florida, or Arizona (Use IN keyword)
+-- Insert an artist to the database
+-- Insert yourself as a customer to the database
+-- Find a list of all Playlists that start with Classical
+-- You can either continue exploring this dataset or look into setting up postgres on your local machine.
